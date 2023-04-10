12f262da1e7c8964858d50438286f22d5395d605
Jake Olkin <jolkin@mit.edu>
2023-04-09 20:35:22 -0400

    fixed threshold bug in checker

--- checker.py
+++ checker.py
@@ -3,94 +3,83 @@ import solutions
 import numpy as np
 import solution_vals
 
-def temporal_sol_check(answer):
+def temporal_sol_check(answer, threshold= 0.001):
+    """
+    Checks student answer for temporal loss
+    :param answer: student temporal loss gradient function
+    :return: None, b
+    """
     mapping = np.arange(200).reshape((2, 100)) / 200
     gt = utils_pset.Ground_truth()
 
     images, coords, actions, rewards = gt.run_program(N=5)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.temporal_solution5))
+    assert np.max(np.abs(student_answer - solution_vals.temporal_solution5)) < threshold
 
     images, coords, actions, rewards = gt.run_program(N=10)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.temporal_solution10))
+    assert np.max(np.abs(student_answer - solution_vals.temporal_solution10)) < threshold
 
     images, coords, actions, rewards = gt.run_program(N=15)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.temporal_solution15))
+    assert np.max(np.abs(student_answer - solution_vals.temporal_solution15)) < threshold
 
     images, coords, actions, rewards = gt.run_program(N=20)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.temporal_solution20))
+    assert np.max(np.abs(student_answer - solution_vals.temporal_solution20)) < threshold
 
-def causal_sol_check(answer):
+def causal_sol_check(answer, threshold = 0.001):
     mapping = np.arange(200).reshape((2, 100)) / 200
     gt = utils_pset.Ground_truth()
 
     images, coords, actions, rewards = gt.run_program(N=5)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.causal_solution5))
+    assert np.max(np.abs(student_answer - solution_vals.causal_solution5)) < threshold
 
     images, coords, actions, rewards = gt.run_program(N=10)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.causal_solution10))
+    assert np.max(np.abs(student_answer - solution_vals.causal_solution10)) < threshold
 
     images, coords, actions, rewards = gt.run_program(N=15)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.causal_solution15))
+    assert np.max(np.abs(student_answer - solution_vals.causal_solution15)) < threshold
 
     images, coords, actions, rewards = gt.run_program(N=20)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.causal_solution20))
+    assert np.max(np.abs(student_answer - solution_vals.causal_solution20)) < threshold
 
 
-def repeatability_sol_check(answer):
+def repeatability_sol_check(answer, threshold = 0.001 ):
     mapping = np.arange(200).reshape((2, 100)) / 200
     gt = utils_pset.Ground_truth()
 
     images, coords, actions, rewards = gt.run_program(N=5)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.repeatability_solution5))
+    assert np.max(np.abs(student_answer - solution_vals.repeatability_solution5)) < threshold
 
     images, coords, actions, rewards = gt.run_program(N=10)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.repeatability_solution10))
+    assert np.max(np.abs(student_answer - solution_vals.repeatability_solution10)) < threshold
 
     images, coords, actions, rewards = gt.run_program(N=15)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.repeatability_solution15))
+    assert np.max(np.abs(student_answer - solution_vals.repeatability_solution15)) < threshold
 
     images, coords, actions, rewards = gt.run_program(N=20)
     frames = utils_pset.raw_to_frame(images, coords, actions, rewards)
     student_answer = answer(frames, mapping)
-    assert np.max(np.abs(student_answer - solution_vals.repeatability_solution20))
-
-if __name__ == "__main__":
-    f = open("solution_vals.py", "w")
-    mapping = np.arange(200).reshape((2, 100))/200
-    for i in range(5,25,5):
-        sol = check_solution(solutions.temporal_cohesion_sol, solutions.temporal_cohesion_sol, mapping, i=i)
-        f.write("temporal_solution{} = np.{}\n".format(i, repr(sol)))
-
-    for i in range(5,25,5):
-        sol = check_solution(solutions.causality_prior_sol, solutions.causality_prior_sol, mapping, i=i)
-        f.write("causal_solution{} = np.{}\n".format(i, repr(sol)))
-
-    for i in range(5,25,5):
-        sol = check_solution(solutions.repeatability_prior_sol, solutions.repeatability_prior_sol, mapping, i=i)
-        f.write("repeatability_solution{} = np.{}\n".format(i, repr(sol)))
-
+    assert np.max(np.abs(student_answer - solution_vals.repeatability_solution20)) < threshold
 
 
 
